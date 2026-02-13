56dee8572b5f3e673bee142bfca0266ae609a1e3
Valentine Creator <valentine@example.com>
2026-02-13 16:17:27 +0530

    fix: update express 5 catch-all route syntax

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.get('/api/responses', (req, res) => {
 
 
 // Handle any requests that don't match the ones above
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
